7b481afc50c14d6fb88562cebf62b0c6805578c3
jonkire <erikjonatha.xii@gmail.com>
2025-05-23 10:59:22 -0300

    atividade ls

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const main = document.querySelector('main');
 function modelFlag(flag) {
   return `
   <div class="flag col-2 my-2 text-center">
-    <img src="${flag.image}" alt="Andorra">
+    <img src="${flag.image}" alt="${flag.name}">
     <p>${flag.name}</p>
   </div>
   `;
